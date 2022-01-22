fbf2a6bcf487e782dbd993eb522216a934d06999
Aylan de Sousa Boscarino <aylandesousa@gmail.com>
2022-01-22 13:23:11 -0300

    basic search functionality

--- index.go
+++ index.go
@@ -14,24 +14,39 @@ type Document struct {
 }
 
 type InvertedIndex struct {
-    m map[string][]Document
+    m map[string][]*Document
 }
 
 func NewInvertedIndex() InvertedIndex {
-    return InvertedIndex{make(map[string][]Document)}
+    var i InvertedIndex
+    i.m = make(map[string][]*Document)
+    return i
+    //return InvertedIndex{m:= make(map[string][]Document, d:= make([]Document))}
 }
 
-func (i *InvertedIndex) indexDocumentWithToken(token string, d Document) {
+func (i *InvertedIndex) indexDocumentWithToken(token string, d *Document) {
     docs, isSet:= i.m[token]
     if isSet == false {
-        i.m[token] = []Document{}
+        i.m[token] = []*Document{}
     }
     i.m[token] = append(docs, d)
 }
 
 func (i *InvertedIndex) insertDocument(d Document) {
     for _, token := range d.tokens {
-        i.indexDocumentWithToken(token.value, d)
+        i.indexDocumentWithToken(token.value, &d)
+    }
+}
+
+func (i *InvertedIndex) findByToken(t string) {
+    docs, _ := i.m[t]
+    if docs != nil {
+        fmt.Println("Found document")
+        for _, doc := range docs {
+            fmt.Println(doc.content)
+        }
+    } else {
+        fmt.Println("Document not found")
     }
 }
 
@@ -48,5 +63,5 @@ func main() {
         },
     }
     i.insertDocument(d)
-    fmt.Println(i.m)
+    i.findByToken("abunda")
 }
